cb4334c0ff675eab68cc3091e51897fb2b837014
HyukJung-Song <go_go_ssing@naver.com>
2021-03-31 23:12:16 +0900

    .

--- 기타/src/B.java
+++ 기타/src/B.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class C {
+public class B {
 
 	public static void main(String[] args) {
 		
---
+++ 기타/src/Java_vs_Python.java
@@ -0,0 +1,11 @@
+
+public class Java_vs_Python {
+
+	public static void main(String[] args) {
+
+		// 1) 배열 더하기는 안됨
+		int[] ar = {1,2};
+		int[] ar2 = {3,4};
+		// int[] arr3 = arr + arr2; (x)
+	}
+}
