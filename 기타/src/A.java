110c65caaa8ec3011477913d38fc3e50f2853288
HyukJung-Song <go_go_ssing@naver.com>
2021-04-04 21:25:52 +0900

    .

--- 기타/src/A.java
+++ 기타/src/A.java
@@ -5,13 +5,18 @@ import java.util.Random;
 public class A {
 
 	public static void main(String[] args) {
-		
-		double num = 2.7582;
-		
-		
 
-
-
+		factorial(5);
 	}
+	
+	// 5! = 5 * 4 * 3 * 2 * 1
+	public static int factorial(int n) {
+		if (n <= 1) {
+			return 1;
+		} else {
+			return n * factorial(n - 1);
+		}
+	}
+
 	
 }
