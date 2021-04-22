099d337d2f7876fd5d461708298664b09acf8a6f
hyukjung <go_go_ssing@naver.com>
2021-04-22 20:01:24 +0900

    .

--- 문제은행/src/sort/Selection_sort.java
+++ 문제은행/src/sort/Selection_sort.java
@@ -6,35 +6,44 @@ import java.util.Random;
 public class Selection_sort {
 
 	public static void main(String[] args) {
+
+		Random r = new Random();
 		
+		// 랜덤배열 생성
 		int[] arr = new int[10];
 		for(int i=0; i<arr.length; i++) {
-			Random r = new Random();
-			int num = r.nextInt(101);
-			arr[i] = num;					
+			arr[i] = r.nextInt(11);					
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		// 선택정렬1 (0기준으로 계속 비교해서 수시로 바꿈. 이게 조금더 빠름. 거의 비슷)
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
 		}
 		
-		selectionSort(arr);
-	}
-	
-	public static void selectionSort(int[] arr) {
-
+		// 선택정렬2 (최소값 index 찾아 한번만 바꿈)
+		int indexMin, tmp;
 		for (int i=0; i<arr.length-1; i++) {
-			int indexMin = i;
+			indexMin = i;
 			for (int j=i+1; j<arr.length; j++) {
 				if (arr[indexMin] > arr[j]) {
 					indexMin = j;
 				}
 			}
-			swap(arr, i, indexMin);
+			tmp = arr[i];
+			arr[i] = arr[indexMin];
+			arr[indexMin] = tmp;
 		}
 		System.out.println("selectionSort 결과: " + Arrays.toString(arr));
-	}
-	
-	public static void swap(int[] arr, int i, int j) {
+
 		
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
+
+
 	}
 }
--- 하던거/src/C.java
+++ 하던거/src/C.java
@@ -1,15 +1,31 @@
+import java.util.Arrays;
+import java.util.Random;
+
 import 정리할것.Stu;
+import 정리할것.Student;
 
-public class C extends Stu{
-
-	C(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
-	
-	
+public class C {
 
 	public static void main(String[] args) {
 		
+		Random r = new Random();
+		
+		int[] arr = new int[10];
+		for (int i=0; i<arr.length; i++) {
+			arr[i] = r.nextInt(11);
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+
 	}
 }
