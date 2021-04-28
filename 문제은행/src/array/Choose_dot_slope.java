da1b20ee5f3c37e34a0cd66ba522bd6191d6317e
hyukjung <go_go_ssing@naver.com>
2021-04-28 15:32:58 +0900

    .

---
+++ 문제은행/src/array/Choose_dot_slope.java
@@ -0,0 +1,23 @@
+package array;
+
+import java.util.Arrays;
+
+public class Choose_dot_slope {
+
+	public static void main(String[] args) {
+
+		int[][] arr = new int[3][];
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		//	arr[0] = {0, 0};
+		
+		int[] arr1 = new int[3];
+		arr1[0] = 1;
+		arr[1] = ;
+		
+		
+		
+	}
+}
