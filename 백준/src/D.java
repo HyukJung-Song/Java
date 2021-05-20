f10b2c9f409008e9ec56bf0d69644b19f28c046d
HyukJung-Song <go_go_ssing@naver.com>
2021-05-20 22:14:51 +0900

    a

--- 백준/src/D.java
+++ 백준/src/D.java
@@ -8,5 +8,19 @@ public class D {
 		int size = Integer.MAX_VALUE;
 		
 		System.out.println(size);
+		
+		long a = 10;
+		a = a + '9' - '0';
+		System.out.println(a);
+		System.out.println('3');
+		System.out.println('0');
+		System.out.println('3'-'0');
+		
+		String s = "";
+		s = "a";
+		System.out.println(s);
+		
+
+		
 	}
 }
--- 백준/src/SMUPC계산기_21737.java
+++ 백준/src/SMUPC계산기_21737.java
@@ -1,99 +1,84 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SMUPC계산기_21737 {
+	
+			
+/*
+ 
+S: -,  M: *,  U: /,  P: +,  C: =  
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+5
+3S2M3U1P2C
+>
+5
+
+
+4
+0328CS00325CC
+>
+328 3 3
+ */
+	
+	static int tmpRes, tmp, calcNo; 
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		// S: -,  M: *,  U: /,  P: +,  C: =  
+		int n = Integer.parseInt(br.readLine());
+		String target = br.readLine();
 		
-		// 3S2C -> 3-2 = 1
-		sc.nextLine();
-		String str = sc.nextLine();
-		String tmp = "";
-		int left, right, res = 0;
-		char target;
-		for (int i=0; i<str.length(); i++) {
-			target = str.charAt(i);
-			
-			if (target == '0') {
-				continue;
-			}
-			
-			if (target == 'C') {
-				System.out.print(tmp + " ");
-				
-			} else if (target == 'S') {
-				left = Integer.parseInt(tmp);
-				i++;
-				tmp = "";
-				if (i == str.length())  break;
-				while(str.charAt(i) != 'S' && str.charAt(i) != 'M' && str.charAt(i) != 'U' && str.charAt(i) != 'P' && str.charAt(i) != 'C') {
-					tmp += str.charAt(i);
-					i++;
+		tmpRes = tmp = calcNo = 0;
+		boolean chk = false;
+		// String res = "";
+		// 골때리는게 res로 하면 백준 통과가 안됨. String보다 StringBuilder가 훨씬 범위가 넒은듯!
+		for (int i=0; i<target.length(); i++) {
+			if (target.charAt(i) == 'S') {
+				calc();
+				calcNo = 1;
+			} else if (target.charAt(i) == 'M') {
+				calc();
+				calcNo = 2;
+			} else if (target.charAt(i) == 'U') {
+				calc();
+				calcNo = 3;	
+			} else if (target.charAt(i) == 'P') {
+				calc();
+				calcNo = 0;
+			} else if (target.charAt(i) == 'C') {
+				calc();
+				calcNo = 0;
+				if (!chk) {
+					sb.append(tmpRes);
+//					res += tmpRes;
+				} else {
+					sb.append(" " + tmpRes);
+//					res += " " + tmpRes;
 				}
-				right = Integer.parseInt(tmp);
-				res = left - right;
-				tmp = Integer.toString(res);
-				i--;
-				
-			} else if (target == 'M') {
-				left = Integer.parseInt(tmp);
-				i++;
-				tmp = "";
-				if (i == str.length())  break;
-				while(str.charAt(i) != 'S' && str.charAt(i) != 'M' && str.charAt(i) != 'U' && str.charAt(i) != 'P' && str.charAt(i) != 'C') {
-					tmp += str.charAt(i);
-					i++;
-				}
-				right = Integer.parseInt(tmp);
-				res = left * right;
-				tmp = Integer.toString(res);
-				i--;
-				
-			} else if (target == 'U') {
-				left = Integer.parseInt(tmp);
-				i++;
-				tmp = "";
-				if (i == str.length())  break;
-				while(str.charAt(i) != 'S' && str.charAt(i) != 'M' && str.charAt(i) != 'U' && str.charAt(i) != 'P' && str.charAt(i) != 'C') {
-					tmp += str.charAt(i);
-					i++;
-				}
-				right = Integer.parseInt(tmp);
-				if (left < 0) {
-					left = left * -1;
-					res = left / right;
-					res = res * -1;
-				} else {					
-					res = left / right;
-				}
-				tmp = Integer.toString(res);
-				i--;
-				
-			} else if (target == 'P') {
-				left = Integer.parseInt(tmp);
-				i++;
-				tmp = "";
-				if (i == str.length())  break;
-				while(str.charAt(i) != 'S' && str.charAt(i) != 'M' && str.charAt(i) != 'U' && str.charAt(i) != 'P' && str.charAt(i) != 'C') {
-					tmp += str.charAt(i);
-					i++;
-				}
-				right = Integer.parseInt(tmp);
-				res = left + right;
-				tmp = Integer.toString(res);
-				i--;
-				
+				chk = true;
 			} else {
-				tmp += target;				
+				tmp = 10*tmp + target.charAt(i) - '0';
 			}
 		}
-		
-		if (tmp.equals("")) {
-			System.out.println("NO OUTPUT");			
+		if (!chk) {
+			System.out.println("NO OUTPUT");
+		} else {
+			System.out.println(sb);
 		}
-		
-		
+	}
+	
+	static void calc() {
+		if (calcNo == 0) {
+			tmpRes += tmp;
+		} else if (calcNo == 1) {
+			tmpRes -= tmp;
+		} else if (calcNo == 2) {
+			tmpRes *= tmp;
+		} else if (calcNo == 3) {
+			tmpRes /= tmp;
+		}
+		tmp = 0;
 	}
 }
