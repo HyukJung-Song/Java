0298fb31df182cfb24f74bc76a23cb3d2813cd88
hyukjung <go_go_ssing@naver.com>
2021-06-02 15:26:15 +0900

    a

--- Number/src/Basic_number.java
+++ Number/src/Basic_number.java
@@ -7,8 +7,8 @@ public class Basic_number {
 		// 헷갈림
 		System.out.println(Integer.parseInt("77"));		// 77
 		System.out.println(Integer.parseInt("077"));	// 77
-		System.out.println(0/2);				// 0
-		System.out.println(0%2);				// 0
+		System.out.println(0/2);				// 0 (몫)
+		System.out.println(0%2);				// 0 (나머지)
 		System.out.println(1 + "1");			// 11
 		
 		
@@ -21,28 +21,9 @@ public class Basic_number {
 		System.out.println();
 		
 		
-		// 삼항연산자
-		for (int i=0; i<2; i++) {
-			String name = (i==0)? "Student1":"Student2";
-			System.out.println(i + " name" + name);
-		}
-		
-		
 		// Random
 		Random rd = new Random();
 		int rdNum = rd.nextInt(100);	// 0~99
-
-		
-		// 증감연산자
-		int a = 1;
-		int b = a++;
-		int c = a;
-		System.out.println("a : " + a);		// 2
-		System.out.println("b : " + b);		// 1	
-		System.out.println("c : " + c);		// 2
-		
-		
-
-		
+	
 	}
 }
